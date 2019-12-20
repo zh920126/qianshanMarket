c23d042a4c71bb4019b5e48aabbfb211b182e20f
zh920126 <53657400+zh920126@users.noreply.github.com>
2019-12-20 21:23:18 +0800

    02-分类页面完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,54 @@
 // pages/category/index.js
+//引入全局的myAxios组件
+const app = getApp();
+//引入async await
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    //定义变量用于渲染右侧页面
+    categories:wx.getStorageSync('categories')||[],
+    //定义变量渲染左侧页面
+    cateItems:wx.getStorageSync('categories')[0]||[],
+    //每次切换类目都让滚动条还原
+    toTop:0,
+    //切换类目时，改变被选中项的颜色
+    choseIndex:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //点击左侧分类栏时触发
+  choseCate(e){
+    //获取每次点击之后得到的索引值
+    let {index}=e.currentTarget.dataset
+    // console.log(index);
+    //每次点击时将对应索引值的数据在左边渲染
+    this.setData({
+      cateItems:this.data.categories[index],
+      toTop:0,
+      choseIndex:index
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  async onLoad(){
+    if(this.data.categories.length===0){
+      //页面一打开就需要获取数据
+    let categories=await app.myAxios({
+      url:"/categories"
+    })
+    // console.log(aa);
+    //因为数据过大 ，每次获取都需要很长时间，因此将该数据存储到本地储存中去
+    wx.setStorageSync("categories", categories);
+    //将数据赋值，用于渲染页面
+    this.setData({
+      categories,
+      cateItems:categories[0]
+    })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 每当页面显示的时候 ，让类目还原到第一个
+  onShow(e){
+    this.setData({
+      toTop:0,
+      choseIndex:0,
+       cateItems:wx.getStorageSync('categories')[0]||[],
+    })
   }
 })
