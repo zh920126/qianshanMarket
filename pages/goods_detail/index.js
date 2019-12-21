66074ec598af2840ca5b9480339c479969955b98
zh920126 <53657400+zh920126@users.noreply.github.com>
2019-12-21 23:11:34 +0800

    07-商品详情页页面数据渲染完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,24 @@
 // pages/goods_detail/index.js
+//引入封装的myAxios
+const app=getApp()
+//引入async 和await
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    //定义变量用于页面的渲染
+    goodDetail:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //打开页面就需要获取到商品的ID，进行数据的获取
+  async onLoad(options){
+    let{goods_id}=options
+    //根据ID来发送请求 获取数据
+    let goodDetail=await app.myAxios({
+      url:"/goods/detail",
+      data:{
+        goods_id:129
+      }
+    })
+    console.log(goodDetail);
+    this.setData({goodDetail})
   }
 })
