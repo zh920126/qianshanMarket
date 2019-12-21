9b3fbc12a692119259e3213bd4d17c5c8c87d13e
zh920126 <53657400+zh920126@users.noreply.github.com>
2019-12-21 18:38:38 +0800

    04-商品列表页面完成

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,81 @@
 // pages/goods_list/index.js
+//引入全局的myAxios
+const app=getApp()
+//引入async和await
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    //定义遍量接收数据，用于渲染页面
+    a:[],
+    goodsList:[],
+    //定义变量存储传送过来的cid
+    cid:0,
+    //定义变量用过来存储传送过来的query
+    query:"",
+    //定义页码
+    pagenum:1,
+    //定义每页显示数据
+    pagesize:4,
+    //定义变量用于获取搜索新的总数据条数
+    total:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //将获取数据的函数封装
+  async getGoodsList(query,cid,pagenum,pagesize){
+    let goodsList=await app.myAxios({
+      url:"/goods/search",
+     data:{
+      query:query,
+      cid:cid,
+      pagenum:pagenum,
+      pagesize:pagesize
+     }
+    })
+    //将数据里面的所有价格数据全部转化为带2个点
+    let newList=goodsList.goods.map(item=>{
+      item.goods_price=item.goods_price.toFixed(2)
+      return item
+    })
+    // console.log(newList);
+    this.data.a.push(...newList)
+    // console.log(this.data.a);
+    // this.data.goodsList.push(...newList)
+    //将获取到的数据赋值给data
+    this.setData({
+      // goodsList:this.goodsList.push(...newList),
+      goodsList:this.data.a,
+      total:goodsList.total
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //根据传入的参数来获取数据
+  async onLoad(options){
+    // console.log(options);
+    //将获取到的 参数解构出来
+    let {query,cid}=options
+    this.setData({
+      query,
+      cid
+    })
+    // console.log(query,cid);
+    //发送请求获取数据
+    //调用函数
+    this.getGoodsList(query,cid,this.data.pagenum,this.data.pagesize)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //上啦加载更多
+  onReachBottom(){
+    ++this.data.pagenum
+    // console.log(this.data.pagenum);
+    // console.log(this.data.total);
+    //调用封装的函数
+    if(this.data.goodsList.length<this.data.total){
+      //如果成立，表示还有数据没有获取完
+      this.getGoodsList(this.data.query,this.data.cid,this.data.pagenum,this.data.pagesize)
+    }else{
+      //提示用户没有更多数据了
+      wx.showToast({
+        title: '没有更多了',
+        icon: 'warn',
+        duration: 1000
+      })
+    }
   }
 })
