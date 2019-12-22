35ed1ee72d440b0931b8d44ca027b159335eb358
zh920126 <53657400+zh920126@users.noreply.github.com>
2019-12-22 11:39:51 +0800

    15-全部收藏tab栏完成

--- pages/collect/index.js
+++ pages/collect/index.js
@@ -11,15 +11,28 @@ Page({
       {name:'店铺收藏'},
       {name:'浏览足迹'}
     ],
+    all:[
+      {name:'全部'},
+      {name:'正在热卖'},
+      {name:'即将上线'}
+    ],
     //定义变量用于tab栏切换
-    isShow:0
+    isShow:0,
+    showRed:0
   },
   //点击顶部tab栏时，切换样式与数据
   choseItem(e){
     let {index}=e.currentTarget.dataset
-    console.log(index);
+    // console.log(index);
     this.setData({
       isShow:index
+    })
+  },
+  //点击全部收藏栏时，切换样式与数据
+  choseCollect(e){
+    let {index}=e.currentTarget.dataset
+    this.setData({
+      showRed:index
     })
   }
 })
