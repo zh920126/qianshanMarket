59b485cede94c83a986e6c768bf743649b38ddd7
zh920126 <53657400+zh920126@users.noreply.github.com>
2019-12-22 11:22:55 +0800

    14-收藏页面顶部tab栏切换完成

--- pages/collect/index.js
+++ pages/collect/index.js
@@ -1,66 +1,25 @@
 // pages/collect/index.js
+//引入全局的myaxios
+const app=getApp()
+//引入async和await
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    collect:[
+      {name:'商品收藏'},
+      {name:'品牌收藏'},
+      {name:'店铺收藏'},
+      {name:'浏览足迹'}
+    ],
+    //定义变量用于tab栏切换
+    isShow:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //点击顶部tab栏时，切换样式与数据
+  choseItem(e){
+    let {index}=e.currentTarget.dataset
+    console.log(index);
+    this.setData({
+      isShow:index
+    })
   }
 })
