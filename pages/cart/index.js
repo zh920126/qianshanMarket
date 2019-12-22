38f8acc8557bad686869310f763ec56f86880342
zh920126 <53657400+zh920126@users.noreply.github.com>
2019-12-22 20:56:20 +0800

    19-购物车页面样式完成

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,54 @@
 // pages/cart/index.js
+//引入封装的axios
+const app=getApp()
+//引入await和async
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    //获取用户信息数据
+    userMessage:{},
+    //获取用户的加入购物车的数据，渲染页面
+    cartList:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //获取用户数据，并将数据改为.00
+  async getNum(){
+    let cartList=await wx.getStorageSync('cart')||[]
+    let newCartList=cartList.map(v=>{
+      v.goods_price=v.goods_price.toFixed(2)
+      return v
+    })
+    this.setData({
+      cartList:newCartList
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //点击-时
+  reduce(e){
+    //获取点击的哪一项的索引值
+    let {index,count}=e.currentTarget.dataset
+    console.log(count);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  //点击+时
+  add(e){
+    //获取点击的哪一项的索引值
+    let {index}=e.currentTarget.dataset
+    // console.log(index);
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //获取用户地址
+  onGotUserInfo(e){
+    // console.log(e);
+    //获取用户授权后，获取到地址信息
+    wx.chooseAddress({
+      success: (res)=>{
+        //将用户信息赋值，用于渲染页面
+        console.log(res);
+        this.setData({
+          userMessage:res
+        })
+      }
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //页面一加载就能调用封装的函数进行赋值
+  onLoad(){
+    this.getNum()
   }
 })
