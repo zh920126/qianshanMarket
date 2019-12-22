6d667f4350b610fd6300cf8aae8cefa022307876
zh920126 <53657400+zh920126@users.noreply.github.com>
2019-12-22 14:15:32 +0800

    16-收藏商品页面完成

--- pages/collect/index.js
+++ pages/collect/index.js
@@ -18,7 +18,9 @@ Page({
     ],
     //定义变量用于tab栏切换
     isShow:0,
-    showRed:0
+    showRed:0,
+    //获取收藏的数据，渲染页面
+    collectList:[]
   },
   //点击顶部tab栏时，切换样式与数据
   choseItem(e){
@@ -33,6 +35,27 @@ Page({
     let {index}=e.currentTarget.dataset
     this.setData({
       showRed:index
+    })
+  },
+  //点击删除时，删除对应的哪一项数据
+  delItem(e){
+    let {index}=e.currentTarget.dataset
+    // console.log(index);
+    //获取本地存储的数据
+    let arr=wx.getStorageSync('collect')
+    arr.splice(index,1)
+    //将新的数组上传到本地存储中去
+    wx.setStorageSync('collect', arr);
+    //删除完之后，进行页面刷新
+    this.onLoad()
+  },
+  //页面一打开就获取到用户收藏的数据
+  onLoad(){
+    //从本地存储中获取用户的收藏数据
+    let arr=wx.getStorageSync('collect');
+    //将数据赋值给定义好的数据，用于渲染页面
+    this.setData({
+      collectList:arr
     })
   }
 })
