4c2d49c5f844e294763685566821f42bdd997539
zh920126 <53657400+zh920126@users.noreply.github.com>
2019-12-24 22:14:31 +0800

    23-搜索页面基本功能完成

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,88 @@
 // pages/search/index.js
+//引入全局的myAxios
+const app=getApp()
+//引入async和await
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    //是否显示字体图标
+    showIcon:true,
+    goodsList:[],
+    value:"",
+    historyList:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //当获得焦点的时候
+  getFocus(){
+    //首先就需要让字体图标消失
+    this.setData({
+      showIcon:false
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //失去焦点时
+  blurFoucs(){
+    this.setData({
+      showIcon:true
+    })
+    this.showHistory()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  //监听输入框内容
+   getList(e){
+    // console.log(e);
+    //获取输入的内容
+    let {value}=e.detail
+    //调用axios
+    app.myAxios({
+      url:"/goods/qsearch",
+      data:{
+        query:value
+      }
+    }).then(res=>{
+      console.log(res);
+      //将获取到的结果赋值变量用于渲染页面
+      //同时将数据赋值给定义的变量
+      this.setData({
+        goodsList:res,
+        value
+      })
+    })
+  } ,
+  //点击进行跳转
+  gtToDetail(e){
+    console.log(e);
+    let {goods_id}=e.currentTarget.dataset
+    wx.navigateTo({
+      url: `/pages/goods_detail/index?goods_id=${goods_id}`,
+      success: (res)=>{
+        // console.log(res);
+        //每次用户点击之后，就将用户输入的数据存起来
+        let arr= wx.getStorageSync('history')||[];
+        arr.push(this.data.value)
+        wx.setStorageSync('history', arr);
+      }
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //封装显示历史记录的函数
+  showHistory(){
+    let historyList=wx.getStorageSync('history')||[];
+    this.setData({
+      historyList
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onLoad(){
+    this.showHistory()
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //清空历史记录
+  delHistory(){
+    wx.removeStorageSync('history');
+    //同时提示用户删除成功
+    wx.showToast({
+      title: '删除历史记录成功',
+      icon: 'success',
+      duration: 1500,
+      mask: true
+    });
+    this.setData({
+      historyList:[]
+    })
   }
 })
